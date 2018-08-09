80c8b3f9f83ff7b0f170718b2ef5e39a6cfc3168
Luis Castellanos <blackizlu@hotmail.com>
2018-08-09 15:37:14 -0500

    nuevo
    s

---
+++ database/migrations/2018_08_09_192731_create_profile_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('name');
+            $table->string(('last_name'));
+            $table->date('birthdate');
+            $table->integer('phone_number');
+            $table->string('color');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
